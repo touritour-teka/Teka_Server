f437afefff812c9567e1fcc7aee4438fe76f985b
jyj1289 <jin37091289@gmail.com>
2025-04-02 14:14:00 +0900

    initialize

---
+++ src/main/java/com/teka/TekaApplication.java
@@ -0,0 +1,13 @@
+package com.teka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teka/TekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
