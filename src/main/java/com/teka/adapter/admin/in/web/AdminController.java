0d5b7430c0036d278225f009ab6a2383d1bc1db1
jyj1289 <jin37091289@gmail.com>
2025-04-15 12:14:38 +0900

    refactor(#1): 불필요한 import 제거

--- src/main/java/com/teka/adapter/admin/in/web/AdminController.java
+++ src/main/java/com/teka/adapter/admin/in/web/AdminController.java
@@ -4,9 +4,11 @@ import com.teka.adapter.admin.in.web.dto.request.SignUpAdminRequest;
 import com.teka.application.admin.port.in.SignUpAdminUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
