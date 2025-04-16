4f42887796c2f7939f2346846be6f6ec58e990bd
jyj1289 <jin37091289@gmail.com>
2025-04-17 01:53:36 +0900

    fix(#3): 생성자 language 인자추가

--- src/main/java/com/teka/domain/user/User.java
+++ src/main/java/com/teka/domain/user/User.java
@@ -4,7 +4,9 @@ import com.teka.domain.chatroom.ChatRoomId;
 import com.teka.domain.user.type.Language;
 import com.teka.domain.user.type.UserType;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class User {
 
     private final UserId id;
@@ -22,11 +24,12 @@ public class User {
     private final ChatRoomId chatRoomId;
 
     @Builder
-    public User(UserId id, String username, PhoneNumber phoneNumber, Email email, UserType type, ChatRoomId chatRoomId) {
+    public User(UserId id, String username, PhoneNumber phoneNumber, Email email, Language language, UserType type, ChatRoomId chatRoomId) {
         this.id = id;
         this.username = username;
         this.phoneNumber = phoneNumber;
         this.email = email;
+        this.language = language;
         this.type = type;
         this.chatRoomId = chatRoomId;
     }
