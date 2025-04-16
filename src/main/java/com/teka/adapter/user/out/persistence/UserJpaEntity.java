31357abab49c3ef21901de270201548ce084a4a8
jyj1289 <jin37091289@gmail.com>
2025-04-17 01:54:31 +0900

    fix(#3): 컬럼 제약 조건 변경 및 from 메서드 생성 - 유저 등록 후 채팅방 참가시 username과 language를 선택하기 때문에 nullable이 되도록 변경했어요.

--- src/main/java/com/teka/adapter/user/out/persistence/UserJpaEntity.java
+++ src/main/java/com/teka/adapter/user/out/persistence/UserJpaEntity.java
@@ -1,24 +1,31 @@
 package com.teka.adapter.user.out.persistence;
 
 import com.teka.adapter.chatroom.out.persistence.ChatRoomJpaEntity;
+import com.teka.domain.chatroom.ChatRoomId;
+import com.teka.domain.user.Email;
+import com.teka.domain.user.PhoneNumber;
+import com.teka.domain.user.User;
+import com.teka.domain.user.UserId;
 import com.teka.domain.user.type.Language;
 import com.teka.domain.user.type.UserType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "tbl_user")
 public class UserJpaEntity {
 
-    @Column
+    @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = true, length = 30)
     private String username;
 
     @Column(nullable = false, length = 11)
@@ -28,7 +35,7 @@ public class UserJpaEntity {
     private String email;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 30)
+    @Column(nullable = true, length = 30)
     private Language language;
 
     @Enumerated(EnumType.STRING)
@@ -37,16 +44,39 @@ public class UserJpaEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false, name="chat_room_id")
-    private ChatRoomJpaEntity chatroom;
+    private ChatRoomJpaEntity chatRoom;
 
     @Builder
-    public UserJpaEntity(Long id, String username, String phoneNumber, String email, Language language, UserType type, ChatRoomJpaEntity chatroom) {
+    public UserJpaEntity(Long id, String username, String phoneNumber, String email, Language language, UserType type, ChatRoomJpaEntity chatRoom) {
         this.id = id;
         this.username = username;
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.language = language;
         this.type = type;
-        this.chatroom = chatroom;
+        this.chatRoom = chatRoom;
+    }
+
+    public static UserJpaEntity from(User user, ChatRoomJpaEntity chatRoom) {
+        return UserJpaEntity.builder()
+                .username(user.getUsername())
+                .phoneNumber(user.getPhoneNumber().value())
+                .email(user.getEmail().value())
+                .language(user.getLanguage())
+                .type(user.getType())
+                .chatRoom(chatRoom)
+                .build();
+    }
+
+    public User toDomain() {
+        return User.builder()
+                .id(new UserId(this.id))
+                .username(this.username)
+                .phoneNumber(new PhoneNumber(this.phoneNumber))
+                .email(new Email(this.email))
+                .language(this.language)
+                .type(this.type)
+                .chatRoomId(new ChatRoomId(this.chatRoom.getId()))
+                .build();
     }
 }
