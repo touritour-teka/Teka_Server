14f4fc446a74279759d4c62f995daa3fc1d65f22
jyj1289 <jin37091289@gmail.com>
2025-04-16 20:28:46 +0900

    feat(#3): NotFound 핸들링 - 잘못된 엔드포인트 요청시 예외가 발생하여 이를 404 NotFound로 핸들링했어요.

--- src/main/java/com/teka/shared/error/GlobalErrorProperty.java
+++ src/main/java/com/teka/shared/error/GlobalErrorProperty.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GlobalErrorProperty implements ErrorProperty {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 알 수 없는 에러가 발생했습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. 다시 한 번 확인해 주세요.");
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. 다시 한 번 확인해 주세요."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 리소스를 찾을 수 없습니다.")
+    ;
 
     private final HttpStatus status;
     private final String message;
