7cafed719f5aaf81dc8b881ecbfd552fa2b2639e
cabbage16 <qocn16@gmail.com>
2025-04-28 00:19:25 +0900

    feat(#6): 로그인 시 유저 정보를 가져오는 기능 구현 - AuthenticationArgumentResolver에서 User와 Admin을 구분하여 AuthFacade에서 정보를 가져오도록 했습니다.

--- src/main/java/com/teka/application/auth/service/AuthFacade.java
+++ src/main/java/com/teka/application/auth/service/AuthFacade.java
@@ -3,7 +3,11 @@ package com.teka.application.auth.service;
 import com.teka.application.admin.exception.AdminNotFoundException;
 import com.teka.application.admin.port.out.FindAdminPort;
 import com.teka.application.auth.port.out.TokenProvider;
+import com.teka.application.user.exception.UserNotFoundException;
+import com.teka.application.user.port.out.FindUserPort;
 import com.teka.domain.admin.Admin;
+import com.teka.domain.user.User;
+import com.teka.domain.user.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +17,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthFacade {
 
     private final FindAdminPort findAdminPort;
+    private final FindUserPort findUserPort;
     private final TokenProvider tokenProvider;
 
     @Transactional(readOnly = true)
-    public Admin getAdmin(String username) {
-        return findAdminPort.findByUsername(tokenProvider.getUuid(username))
+    public Admin getAdmin(String token) {
+        return findAdminPort.findByUsername(tokenProvider.getUuid(token))
                 .orElseThrow(AdminNotFoundException::new);
+    }
+
+    @Transactional(readOnly = true)
+    public User getUser(String token) {
+        return findUserPort.findById(new UserId(Long.valueOf(tokenProvider.getUuid(token))))
+                .orElseThrow(UserNotFoundException::new);
     }
 }
