69adca2b8eb0e8e33de68e123d4754fb9224758e
cabbage16 <qocn16@gmail.com>
2025-04-28 09:19:00 +0900

    refactor: 사용되지 않는 import문 제거

--- src/main/java/com/teka/application/chatroom/service/DeleteUserService.java
+++ src/main/java/com/teka/application/chatroom/service/DeleteUserService.java
@@ -1,10 +1,9 @@
 package com.teka.application.chatroom.service;
 
 import com.teka.application.chatroom.exception.ChatRoomNotFoundException;
-import com.teka.application.chatroom.port.out.FindChatRoomPort;
-import com.teka.domain.chatroom.exception.ChatRoomAdminMismatchException;
 import com.teka.application.chatroom.port.in.DeleteUserUseCase;
 import com.teka.application.chatroom.port.in.command.DeleteUserCommand;
+import com.teka.application.chatroom.port.out.FindChatRoomPort;
 import com.teka.application.user.port.out.DeleteUserPort;
 import com.teka.domain.admin.AdminId;
 import com.teka.domain.chatroom.ChatRoomId;
