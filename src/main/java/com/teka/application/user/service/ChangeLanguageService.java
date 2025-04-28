cf39af440e8f9b420e5ae4f971365ee7acbb880a
cabbage16 <qocn16@gmail.com>
2025-04-28 15:12:59 +0900

    perf(#6): ChangeLanguageService 쿼리 성능 개선 - FindUserPort로 User의 존재를 검증할 필요없이 changeLanguage를 실행하는 도중 에러가 발생할 경우 에러 처리를 하는 식으로 성능을 개선했어요.

--- src/main/java/com/teka/application/user/service/ChangeLanguageService.java
+++ src/main/java/com/teka/application/user/service/ChangeLanguageService.java
@@ -4,8 +4,8 @@ import com.teka.application.user.exception.UserNotFoundException;
 import com.teka.application.user.port.in.ChangeLanguageUseCase;
 import com.teka.application.user.port.in.command.ChangeLanguageCommand;
 import com.teka.application.user.port.out.ChangeUserPort;
-import com.teka.application.user.port.out.FindUserPort;
 import com.teka.domain.user.UserId;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +13,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChangeLanguageService implements ChangeLanguageUseCase {
 
-    private final FindUserPort findUserPort;
     private final ChangeUserPort changeUserPort;
 
     @Override
     public void execute(UserId userId, ChangeLanguageCommand command) {
-        findUserPort.findById(userId)
-                .orElseThrow(UserNotFoundException::new);
-        changeUserPort.changeLanguage(userId, command.language());
+        try {
+            changeUserPort.changeLanguage(userId, command.language());
+        } catch (EntityNotFoundException e) {
+            throw new UserNotFoundException();
+        }
     }
 }
