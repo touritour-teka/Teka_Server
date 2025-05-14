b9332b3306f6b3eb334dfc14e5e2e3f41aa5f104
cabbage16 <qocn16@gmail.com>
2025-05-14 19:43:18 +0900

    feat(#9): WebSocket, Redis 설정 - WebSocket과 Redis Pub/Sub 기능을 사용하기 위해서 설정했어요.

---
+++ src/main/java/com/teka/shared/config/WebSocketConfig.java
@@ -0,0 +1,36 @@
+package com.teka.shared.config;
+
+import com.teka.shared.auth.WebSocketAuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final WebSocketAuthenticationInterceptor webSocketAuthenticationInterceptor;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/conn")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(webSocketAuthenticationInterceptor);
+    }
+}
